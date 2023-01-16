01d5c152d6e266dc82f041f9f42cbe9fc10f1999
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-01-16 13:10:15 +0700

    first git

---
+++ app/Views/mobc/detail.php
@@ -0,0 +1,4 @@
+<?= $this->extend('template') ?>
+<?= $this->section('content') ?>
+<h1>detail profile agen obc</h1>
+<?= $this->endSection() ?>
---
+++ app/Views/mvisit/edit.php
@@ -0,0 +1,4 @@
+<?= $this->extend('template') ?>
+<?= $this->section('content') ?>
+<h1>edit</h1>
+<?= $this->endSection() ?>
