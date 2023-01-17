9de9c334fb9bfd2fde9dc9d9f257746d650f3960
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-01-17 16:48:40 +0700

    update view caring detail

--- app/Views/mcaring/detail.php
+++ app/Views/mcaring/detail.php
@@ -2,10 +2,191 @@
 <?= $this->section('content') ?>
 
 <div class="container-fluid py-4">
-    <div class="row">
+    <div class="ms-auto my-auto mt-lg-0 mt-4">
+        <div class="ms-auto my-auto">
+            <a href="./new-product.html" class="btn bg-gradient-primary  mb-0" target="_blank"> <i class="material-icons">edit</i> Edit </a>
+            <button type="button" class="btn btn-outline-primary  mb-0" data-bs-toggle="modal" data-bs-target="#import">
+                Import
+            </button>
+            <div class="modal fade" id="import" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog mt-lg-10">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="ModalLabel">Import CSV</h5>
+                            <i class="material-icons ms-3">file_upload</i>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>You can browse your computer for a file.</p>
+                            <div class="input-group input-group-dynamic mb-3">
+                                <label class="form-label">Browse file...</label>
+                                <input type="email" class="form-control" onfocus="focused(this)" onfocusout="defocused(this)">
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="importCheck" checked="">
+                                <label class="custom-control-label" for="importCheck">I accept the terms and conditions</label>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn bg-gradient-secondary " data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn bg-gradient-primary ">Upload</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-outline-primary  export mb-0 mt-sm-0 mt-1" data-type=".xlsx" type="button" name="button">Export</button>
+        </div>
+    </div>
+
+    <table class="mt-5">
+        <tr class="row">
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Nomor Jastel Pelanggan</p>
+                            <span class="text-success text-sm font-weight-bolder">xxxx</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Nama Pelanggan</p>
+                            <span class="text-success text-sm font-weight-bolder">xxxx</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Kontak Telepon Pelanggan</p>
+                            <span class="text-success text-sm font-weight-bolder">xxxx</span>
+                            <p class="text-sm mb-0 text-capitalize">Agen Pengelola</p>
+                            <span class="text-success text-sm font-weight-bolder">xxxx</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Produk Pelanggan</p>
+                            <span class="text-success text-sm font-weight-bolder">xxxx</span><br>
+                        </div>
+                    </div>
+
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Alamat Lengkap Pelanggan</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+                            </p>
+                            <p class="text-sm mb-0 text-capitalize">Status Call</p>
+                            <span class="text-success text-sm font-weight-bolder">Contacted / Not Contacted</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Tanggal Caring</p>
+                            <span class="text-success text-sm font-weight-bolder">04 - 04 - 2002</span><br>
+                        </div>
+                    </div>
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Reason Call</p>
+                            <span class="text-success text-sm font-weight-bolder">...dropdown....</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Penerima Telepon</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">YBS / Bukan YBS</span>
+                            </p>
+                            <p class="text-sm mb-0 text-capitalize">Hubungan Dengan YBS</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">adik / paman/ orangtua</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr class="row mt-5">
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Kendala Pelanggan</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+                            </p>
+                            <p class="text-sm mb-0 text-capitalize">Hasil Caring</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Reason Call</p>
+                            <span class="text-success text-sm font-weight-bolder">...dropdown....</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Hasil Caring</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+            <td style="width: 400px;">
+                <div class="card">
+                    <div class="card-header py-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons text-white opacity-10">
+                                account_circle
+                            </i>
+                        </div>
+                        <div class="mt-5">
+                            <p class="text-sm mb-0 text-capitalize">Reason Call</p>
+                            <span class="text-success text-sm font-weight-bolder">...dropdown....</span><br>
+                            <p class="text-sm mb-0 text-capitalize">Hasil Caring</p>
+                            <p class="pb-0 mb-0">
+                                <span class="text-success text-sm font-weight-bolder ">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card-footer p-3">
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <!-- <div class="row">
         <div class="col-lg-6 col-12">
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-12 mb-4">
+                <div class="col-lg-6 col-12 col-md-6 mb-4">
                     <div class="card">
                         <div class="card-header p-3 py-2">
                             <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
@@ -13,39 +194,42 @@
                                     account_circle
                                 </i>
                             </div>
-                            <div class="text-end pt-1">
-                                <p class="text-sm mb-0 text-capitalize">Users Active</p>
-                                <h4 class="mb-0">1,600</h4>
+                            <div class="text-end mt-5" style="padding-bottom:81.4px">
+                                <p class="text-sm mb-0 text-capitalize">Status Call</p>
+                                <span class="text-success text-sm font-weight-bolder">status call</span><br><br>
+                                <p class="text-sm mb-0 text-capitalize">Penerima Call</p>
+                                <span class="text-success text-sm font-weight-bolder">penerima call</span><br>
                             </div>
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than lask week</p>
+                            <p class="mb-0"> </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-12 mb-4">
+                <div class="col-lg-6 col-12 col-md-6 mb-4">
                     <div class="card">
                         <div class="card-header p-3 py-2">
                             <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
                                 <i class="material-icons text-white opacity-10">
-                                    pan_tool
+                                    account_circle
                                 </i>
                             </div>
-                            <div class="text-end pt-1">
-                                <p class="text-sm mb-0 text-capitalize">Click Events</p>
-                                <h4 class="mb-0">357</h4>
+                            <div class="text-end mt-5">
+                                <p class="text-sm mb-0 text-capitalize">Alamat Pelanggan</p>
+                                <span class="text-success text-sm font-weight-bolder" style="text-align: justify;">Jalan Gotong Royong No.53 RT.03 RW.03 Kelurahan Karang Raja Kecamatan Prabumulih Timur Kota Prabumulih Sumatera Selatan 3111</span><br>
+
                             </div>
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+124% </span>than lask week</p>
+
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row mt-2">
-                <div class="col-lg-6 col-md-6 col-12">
+                <div class="col-lg-6 col-12" col-md-6>
                     <div class="card">
                         <div class="card-header p-3 py-2">
                             <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
@@ -64,7 +248,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-12 mt-4 mt-md-0">
+                <div class="col-lg-6 col-12 col-md-6 mt-4 mt-md-0">
                     <div class="card">
                         <div class="card-header p-3 py-2">
                             <div class="icon icon-lg icon-shape bg-gradient-info shadow text-center border-radius-xl mt-n4 position-absolute">
@@ -85,67 +269,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 col-12 mt-4 mt-lg-0">
-            <div class="card h-100">
-                <div class="card-header pb-0 p-3">
-                    <h6 class="mb-0">Reviews</h6>
-                </div>
-                <div class="card-body pb-0 p-3">
-                    <ul class="list-group">
-                        <li class="list-group-item border-0 d-flex align-items-center px-0 mb-0">
-                            <div class="w-100">
-                                <div class="d-flex mb-2">
-                                    <span class="me-2 text-sm text-capitalize">Positive reviews</span>
-                                    <span class="ms-auto text-sm">80%</span>
-                                </div>
-                                <div>
-                                    <div class="progress progress-md">
-                                        <div class="progress-bar bg-gradient-info w-80" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                            <div class="w-100">
-                                <div class="d-flex mb-2">
-                                    <span class="me-2 text-sm text-capitalize">Neutral reviews</span>
-                                    <span class="ms-auto text-sm">17%</span>
-                                </div>
-                                <div>
-                                    <div class="progress progress-md">
-                                        <div class="progress-bar bg-gradient-dark w-10" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                            <div class="w-100">
-                                <div class="d-flex mb-2">
-                                    <span class="me-2 text-sm text-capitalize">Negative reviews</span>
-                                    <span class="ms-auto text-sm">3%</span>
-                                </div>
-                                <div>
-                                    <div class="progress progress-md">
-                                        <div class="progress-bar bg-gradient-danger w-5" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="card-footer pt-0 p-3 d-flex align-items-center">
-                    <div class="w-60">
-                        <p class="text-sm mb-0">
-                            More than <b>1,500,000</b> developers used Creative Tim's products and over <b>700,000</b> projects were created.
-                        </p>
-                    </div>
-                    <div class="w-40 text-end">
-                        <a class="btn bg-gradient-dark mb-0" href="javascript:;">View all reviews</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    </div> -->
 
 
 
