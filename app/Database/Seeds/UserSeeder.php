9d2305bd1a6910c93830633f8d79505157e0572e
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-01-24 09:46:22 +0700

    add admin account di seeder

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'username' => 'admin',
+            'password'    => password_hash('admin', PASSWORD_BCRYPT),
+        ];
+        $this->db->table('admin')->insertBatch($data);
+    }
+}
