181f7144984c855928967d5d0ad195a8fb497deb
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-02-22 18:05:20 +0700

    Update template.php

--- app/Views/template.php
+++ app/Views/template.php
@@ -240,8 +240,7 @@
                 <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                     <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                         <div class="input-group input-group-outline">
-                            <label class="form-label">Search here</label>
-                            <input type="text" class="form-control">
+                            
                         </div>
                     </div>
                     <ul class="navbar-nav  justify-content-end">
@@ -457,7 +456,7 @@
         });
 
         // Bar chart
-        
+
         new Chart(ctx3, {
             type: "bar",
             data: {
