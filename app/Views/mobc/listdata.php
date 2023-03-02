c7148d13e52535a24666d6d9ffb5c0c37a86cbe3
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-03-02 14:36:06 +0700

    Update listdata.php

--- app/Views/mobc/listdata.php
+++ app/Views/mobc/listdata.php
@@ -4,6 +4,36 @@
 
 <div class="container-fluid py-4 ">
     <div class="ms-auto my-auto mt-lg-0 ">
+
+        <div class="alert alert-info text-white" role="alert">
+            <span class="alert-icon align-middle">
+                <span class="material-icons text-md">
+                    info
+                </span>
+            </span>
+            <span class="alert-text"><strong>Data Berikut Hanya Untuk Caring 1 Bulan.</strong> Dikhususkan Untuk Agen Outbound Call (OBC)</span>
+        </div>
+
+        <?php
+        if ($hitung == null) {
+            $statuscall = 0;
+            $total = 0;
+        ?>
+            <div class="alert alert-warning alert-dismissible text-white fade show" role="alert">
+                <span class="alert-icon align-middle">
+                    <span class="material-icons text-md">
+                        warning
+                    </span>
+                </span>
+                <span class="alert-text"><strong>Mohon Import Data Lebih Dahulu! di <a style="color: white;" href="/datapelanggan/caring/satubulan"><u>sini</u></a></strong> Karena data masih kosong</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php
+        }
+        ?>
+
         <div class="row">
             <div class="col-12">
                 <div class="card my-4">
@@ -22,13 +52,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php
-                                    foreach ($hitung as $row) {
-                                        // $a = $row['profil_kesepakatan'];
-                                        // $b = $row['Jumlah_Customer'];
-
-                                    ?>
-
+                                    <?php foreach ($hitung as $row) { ?>
                                         <tr>
 
                                             <td class="align-middle text-center text-sm">
@@ -37,7 +61,6 @@
                                             <td class="align-middle text-center text-sm">
                                                 <h6 class="me-2 text-xs font-weight-bold "><?php $number = number_format($row->total / $seluruh, 2) * 100 ?>
                                                     <?php echo "$number %" ?></h6>
-
                                             </td>
                                         </tr>
                                     <?php
@@ -57,7 +80,7 @@
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3">Data Agen Payment Collection</h6>
+                            <h6 class="text-white text-capitalize ps-3">Data Agen Outbound Call</h6>
                         </div>
                     </div>
                     <div class="card-body px-0 pb-2">
